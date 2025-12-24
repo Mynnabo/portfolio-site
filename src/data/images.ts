1671d2fa075e9b283e023ee4f4febf65cafa3fa0
Mynnabo <mynnabostudio@gmail.com>
2025-12-24 18:37:22 +0100

    images navigation

--- src/data/images.ts
+++ src/data/images.ts
@@ -14,7 +14,7 @@ export const galleryImages: ImageItem[] = [
     url: 'https://pub-d10472e1ed20439a8d45da80f3e9a86b.r2.dev/rouge-gorge.webp',
     width: 1000,
     height: 600,
-    alt: 'Rouge-Gorge',
+    alt: 'Rouge-Gorge — 500mm · 1/1600 · f/5.6 · ISO 6400',
     category: 'Birds'
   },
   {
@@ -22,7 +22,7 @@ export const galleryImages: ImageItem[] = [
     url: 'https://pub-d10472e1ed20439a8d45da80f3e9a86b.r2.dev/cormoran.webp',
     width: 1000,
     height: 600,
-    alt: 'Cormoran Commun',
+    alt: 'Cormoran Commun — 480mm · 1/1600 · f/5.6 · ISO 1800',
     category: 'Birds'
   },
   {
@@ -30,7 +30,7 @@ export const galleryImages: ImageItem[] = [
     url: 'https://pub-d10472e1ed20439a8d45da80f3e9a86b.r2.dev/faucon.webp',
     width: 1000,
     height: 600,
-    alt: 'Faucon Crécerelle',
+    alt: 'Faucon Crécerelle — 500mm · 1/1600 · f/5.6 · ISO 1800',
     category: 'Birds'
   }
 ];
