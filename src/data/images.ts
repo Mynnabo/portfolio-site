16cdca491318c143f0bec38fe2fc7b3cd1d6c4fe
Mynnabo <mynnabostudio@gmail.com>
2025-12-25 22:49:11 +0100

    refine order

--- src/data/images.ts
+++ src/data/images.ts
@@ -10,18 +10,18 @@ export interface ImageItem {
 export const galleryImages: ImageItem[] = [
   {
     id: '11',
-    url: 'https://pub-d10472e1ed20439a8d45da80f3e9a86b.r2.dev/rouge-gorge.webp',
-    width: 1000,
-    height: 600,
-    alt: 'Rouge-Gorge — 500mm · 1/1600 · f/5.6 · ISO 6400',
-    category: 'Birds'
-  },
-  {
-    id: '10',
     url: 'https://pub-d10472e1ed20439a8d45da80f3e9a86b.r2.dev/cormoran.webp',
     width: 1000,
     height: 600,
     alt: 'Cormoran Commun — 480mm · 1/1600 · f/5.6 · ISO 1800',
+    category: 'Birds'
+  },
+  {
+    id: '10',
+    url: 'https://pub-d10472e1ed20439a8d45da80f3e9a86b.r2.dev/rouge-gorge.webp',
+    width: 1000,
+    height: 600,
+    alt: 'Rouge-Gorge — 500mm · 1/1600 · f/5.6 · ISO 6400',
     category: 'Birds'
   },
   {
