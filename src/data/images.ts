32bfefaa65744651f19372aed95317527019d12c
Mynnabo <mynnabostudio@gmail.com>
2025-12-25 22:42:30 +0100

    add new images

--- src/data/images.ts
+++ src/data/images.ts
@@ -8,9 +8,8 @@ export interface ImageItem {
 }
 
 export const galleryImages: ImageItem[] = [
-  // Ligne 1
   {
-    id: '1',
+    id: '11',
     url: 'https://pub-d10472e1ed20439a8d45da80f3e9a86b.r2.dev/rouge-gorge.webp',
     width: 1000,
     height: 600,
@@ -18,7 +17,7 @@ export const galleryImages: ImageItem[] = [
     category: 'Birds'
   },
   {
-    id: '2',
+    id: '10',
     url: 'https://pub-d10472e1ed20439a8d45da80f3e9a86b.r2.dev/cormoran.webp',
     width: 1000,
     height: 600,
@@ -26,11 +25,75 @@ export const galleryImages: ImageItem[] = [
     category: 'Birds'
   },
   {
-    id: '3',
+    id: '9',
     url: 'https://pub-d10472e1ed20439a8d45da80f3e9a86b.r2.dev/faucon.webp',
     width: 1000,
     height: 600,
     alt: 'Faucon Crécerelle — 500mm · 1/1600 · f/5.6 · ISO 1800',
+    category: 'Birds'
+  },
+  {
+    id: '8',
+    url: 'https://pub-d10472e1ed20439a8d45da80f3e9a86b.r2.dev/DSC_4775.webp',
+    width: 1000,
+    height: 600,
+    alt: 'Aigrette Garzette — 500mm · 1/1250 · f/5.6 · ISO 720',
+    category: 'Birds'
+  },
+  {
+    id: '7',
+    url: 'https://pub-d10472e1ed20439a8d45da80f3e9a86b.r2.dev/DSC_4144.webp',
+    width: 1000,
+    height: 600,
+    alt: 'Vautour Fauve — 300mm · 1/800 · f/7.1 · ISO 160',
+    category: 'Birds'
+  },
+  {
+    id: '6',
+    url: 'https://pub-d10472e1ed20439a8d45da80f3e9a86b.r2.dev/DSC_3799.webp',
+    width: 1000,
+    height: 600,
+    alt: 'Pinson des arbres — 220mm · 1/640 · f/5.6 · ISO 160',
+    category: 'Birds'
+  },
+  {
+    id: '5',
+    url: 'https://pub-d10472e1ed20439a8d45da80f3e9a86b.r2.dev/DSC_3794.webp',
+    width: 1000,
+    height: 600,
+    alt: 'Abeille — 300mm · 1/640 · f/6.3 · ISO 160',
+    category: 'Birds'
+  },
+  {
+    id: '4',
+    url: 'https://pub-d10472e1ed20439a8d45da80f3e9a86b.r2.dev/DSC_3527.webp',
+    width: 1000,
+    height: 600,
+    alt: 'Mouette Rieuse — 300mm · 1/1600 · f/6.3 · ISO 4500',
+    category: 'Birds'
+  },
+  {
+    id: '3',
+    url: 'https://pub-d10472e1ed20439a8d45da80f3e9a86b.r2.dev/DSC_3513.webp',
+    width: 1000,
+    height: 600,
+    alt: 'Canard Colvert — 300mm · 1/2000 · f/6.3 · ISO 280',
+    category: 'Birds'
+  },
+  {
+    id: '2',
+    url: 'https://pub-d10472e1ed20439a8d45da80f3e9a86b.r2.dev/DSC_3505.webp',
+    width: 1000,
+    height: 600,
+    alt: 'Aigrette Garzette — 300mm · 1/2500 · f/6.3 · ISO 1250',
+    category: 'Birds'
+  },
+  {
+    id: '1',
+    url: 'https://pub-d10472e1ed20439a8d45da80f3e9a86b.r2.dev/DSC_3463.webp',
+    width: 1000,
+    height: 600,
+    alt: 'Cygne Tuberculé — 300mm · 1/2000 · f/6.3 · ISO 2000',
     category: 'Birds'
   }
 ];
